5e4f458fc33cb6ad87854839543238db3bcd996e
Ahmad Ramzy <ahmadramzy988@gmail.com>
2024-10-20 15:00:30 +0300

    feat(actions): fix how we import assets

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: './',
 };
+
+module.exports = nextConfig;

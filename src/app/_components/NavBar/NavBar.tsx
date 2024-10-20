c33c0481d894faeb5d0bac5a6617c8d94997befb
Ahmad Ramzy <ahmadramzy988@gmail.com>
2024-10-20 15:00:46 +0300

    feat(navBar): fix mobile navBar

--- src/app/_components/NavBar/NavBar.tsx
+++ src/app/_components/NavBar/NavBar.tsx
@@ -6,6 +6,7 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import AnimatedLogo from '@/app/_components/AnimatedLogo';
 import MobileNavBar from '@/app/_components/NavBar/MobileNavBar';
+
 export default function NavBar() {
   const pathname = usePathname();
   const [activeTab, setActiveTab] = useState('Home');
@@ -14,7 +15,7 @@ export default function NavBar() {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const currentPath = pathname.slice(1) || 'home';
+    const currentPath = pathname?.slice(1) || 'home';
     const currentTab = tabs.find((tab) => tab.toLowerCase() === currentPath);
     if (currentTab) {
       setActiveTab(currentTab);
@@ -85,65 +86,69 @@ export default function NavBar() {
 
   return (
     <motion.header initial="visible" animate="visible" variants={navVariants} className="relative">
-      <motion.nav className="flex items-center justify-between p-4">
+      <motion.nav
+        className={`flex items-center ${isMobile ? 'justify-between' : 'justify-center'} p-4`}
+      >
         <AnimatedLogo />
 
-        <div className="flex-grow flex justify-center items-start">
-          <motion.ul
-            className="hidden md:flex space-x-6 list-none m-0 p-2 rounded-full relative shadow-lg bg-white"
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.3 }}
-          >
-            <AnimatePresence>
-              {tabs.map((item) => (
-                <motion.li key={item} className="relative z-10" variants={tabVariants}>
-                  {activeTab === item && (
-                    <motion.div
-                      className="absolute inset-0 bg-[#f94706] rounded-full shadow-md"
-                      layoutId="activeTab"
-                      initial={false}
-                      animate={{
-                        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-                      }}
-                      transition={{
-                        type: 'spring',
-                        stiffness: 300,
-                        damping: 30,
-                        duration: 0.6,
-                      }}
-                    />
-                  )}
+        {!isMobile && (
+          <div className="flex-grow flex justify-center items-start">
+            <motion.ul
+              className="flex space-x-6 list-none m-0 p-2 rounded-full relative shadow-lg bg-white"
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.3 }}
+            >
+              <AnimatePresence>
+                {tabs.map((item) => (
+                  <motion.li key={item} className="relative z-10" variants={tabVariants}>
+                    {activeTab === item && (
+                      <motion.div
+                        className="absolute inset-0 bg-[#f94706] rounded-full shadow-md"
+                        layoutId="activeTab"
+                        initial={false}
+                        animate={{
+                          boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                        }}
+                        transition={{
+                          type: 'spring',
+                          stiffness: 300,
+                          damping: 30,
+                          duration: 0.6,
+                        }}
+                      />
+                    )}
 
-                  <Link
-                    href={`/${item.toLowerCase()}`}
-                    className={`px-4 py-2 relative z-20 block transition-colors duration-300 no-underline ${
-                      activeTab === item ? 'text-white' : 'text-gray-800 hover:text-gray-600'
-                    }`}
-                  >
-                    <motion.span
-                      variants={itemVariants}
-                      initial="initial"
-                      whileHover={{
-                        ...itemVariants.hover,
-                        textShadow: '0 0 8px rgba(255, 255, 255, 0.8)',
-                        color: activeTab === item ? '#ffffff' : '#f94706',
-                      }}
-                      whileTap="tap"
-                      onClick={() => handleTabClick(item)}
-                      animate={activeTab === item ? { rotate: [0, 5, -5, 0] } : {}}
-                      transition={{ duration: 0.8 }}
-                      style={{
-                        color: activeTab === item ? '#ffffff' : 'black',
-                      }}
+                    <Link
+                      href={`/${item.toLowerCase()}`}
+                      className={`px-4 py-2 relative z-20 block transition-colors duration-300 no-underline ${
+                        activeTab === item ? 'text-white' : 'text-gray-800 hover:text-gray-600'
+                      }`}
                     >
-                      {item}
-                    </motion.span>
-                  </Link>
-                </motion.li>
-              ))}
-            </AnimatePresence>
-          </motion.ul>
-        </div>
+                      <motion.span
+                        variants={itemVariants}
+                        initial="initial"
+                        whileHover={{
+                          ...itemVariants.hover,
+                          textShadow: '0 0 8px rgba(255, 255, 255, 0.8)',
+                          color: activeTab === item ? '#ffffff' : '#f94706',
+                        }}
+                        whileTap="tap"
+                        onClick={() => handleTabClick(item)}
+                        animate={activeTab === item ? { rotate: [0, 5, -5, 0] } : {}}
+                        transition={{ duration: 0.8 }}
+                        style={{
+                          color: activeTab === item ? '#ffffff' : 'black',
+                        }}
+                      >
+                        {item}
+                      </motion.span>
+                    </Link>
+                  </motion.li>
+                ))}
+              </AnimatePresence>
+            </motion.ul>
+          </div>
+        )}
 
         <MobileNavBar
           isMenuOpen={isMenuOpen}
